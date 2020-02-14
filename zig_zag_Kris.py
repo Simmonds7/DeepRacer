367b6a7269ade690e7dac3eee241e02375bb67ad
Simmonds7 <kristofer.simmonds@gmail.com>
2020-02-14 09:11:20 -0600

    Car Program 1.0

--- zig_zag_Kris.py
+++ zig_zag_Kris.py
@@ -7,19 +7,23 @@ def reward_function(params):
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
     steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    reward_multiplier = 1
 
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    marker_3 = 0.4 * track_width
 
     # Give higher reward if the agent is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1
+        reward_multiplier += 1.5
+        reward = 1 * reward_multiplier
     elif distance_from_center <= marker_2:
-        reward = 0.5
+        reward_multiplier += 1
+        reward = 0.5 * reward_multiplier
     elif distance_from_center <= marker_3:
-        reward = 0.1
+        reward_multiplier = 1
+        reward = 0.1 * reward_multiplier
     else:
         reward = 1e-3  # likely crashed/ close to off track
 
